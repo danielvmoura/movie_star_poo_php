79bb209324a87a80c47b40ca584457a0cf0f8440
Daniel Moura <daniel31032003@gmail.com>
2025-08-28 09:58:14 -0300

    creating page profile

---
+++ review_process.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("globals.php");
+require_once("db.php");
+require_once("models/Movie.php");
+require_once("models/Message.php");
+require_once("dao/UserDAO.php");
+require_once("dao/MovieDAO.php");
